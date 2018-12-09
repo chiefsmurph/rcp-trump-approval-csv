6d695d5edb29886e277f2e0002d63399cafd9fc3
John Murphy <chiefsmurph@gmail.com>
2018-12-09 13:31:48 -0800

    csv-express: make the request return the json as csv

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const url = 'https://www.realclearpolitics.com/epolls/json/6179_historical.js';
+
+const express = require('express');
+const csv = require('csv-express');
+const app = express();
+
 const rp = require('request-promise');
 
-(async () => {
-
+const getRCPTrumpJSON = async () => {
     const res = await rp(url);
-    console.log(res);
+    const trimmed = res.slice(12).slice(0, -2);
+    const parsed = JSON.parse(trimmed).poll.rcp_avg;
+    const mapped = parsed.map(({ date, candidate: [approve, disapprove] }) => ({
+        date: new Date(date).toLocaleDateString(),
+        approve: approve.value,
+        disapprove: disapprove.value
+    }));
+    return mapped;
+};
 
-})();
+ 
+app.get('/', async (req, res) => {
+    console.log(new Date().toLocaleString(), 'getting rcp trump json', req)
+    const json = await getRCPTrumpJSON();
+    console.log({ json})
+    res.csv(json);
+});
+ 
+app.listen(3010);
+
