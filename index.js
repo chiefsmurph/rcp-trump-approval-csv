8cc172eca5462fcf0f03ad71ed6f221d146ed42c
John Murphy <chiefsmurph@gmail.com>
2018-12-09 15:44:58 -0800

    csv extension

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const get538 = require('./get538');
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/home.html'));
 
-app.get('/rcp', async (req, res) => {
+app.get('/rcp.csv', async (req, res) => {
     console.log(new Date().toLocaleString(), 'getting rcp trump json')
     const json = await getRCP();
     res.csv(json, true);
 });
 
-app.get('/538', async (req, res) => {
+app.get('/538.csv', async (req, res) => {
     const includeFuture = req.query.includeFuture !== undefined;
     console.log(new Date().toLocaleString(), 'getting 538 trump json', { includeFuture })
     const json = await get538(includeFuture);
